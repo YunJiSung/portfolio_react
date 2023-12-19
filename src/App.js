800b95b93bb6d2071b677d7a2c450af3dd201811
YunJiSung <144635669+YunJiSung@users.noreply.github.com>
2023-12-20 07:56:27 +0900

    2023.12.20

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Home from './pages/Home'
+import Nav from './components/Nav'
 
 const App = () => {
   return (
     <>
       <Home />
+      <Nav />
     </>
   )
 }
 
 export default App
+
